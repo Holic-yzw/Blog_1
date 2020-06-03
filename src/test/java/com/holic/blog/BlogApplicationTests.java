dcecb150569f24d957036b6d82e3fb2f8a66c324
Holic-yzw <314649708@qq.com>
2020-06-03 18:33:10 +0800

    后台管理员登陆

--- src/test/java/com/holic/blog/BlogApplicationTests.java
+++ src/test/java/com/holic/blog/BlogApplicationTests.java
@@ -1,6 +1,7 @@
 package com.holic.blog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +9,7 @@ class BlogApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
